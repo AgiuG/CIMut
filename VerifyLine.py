d3a7fa62dffae44a0f05637af793e43cb5eed2ea
Guilherme Silva <guilhermeduarte19@gmail.com>
2025-02-06 20:14:31 -0300

    Subindo codigo

---
+++ VerifyLine.py
@@ -0,0 +1,32 @@
+import paramiko
+
+def get_line_content(host, port, user, password, remote_file_path, line_number):
+    transport = paramiko.Transport((host, port))
+    transport.connect(username=user, password=password)
+
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+    # Download the file
+    local_file_path = 'local_file.txt'
+    sftp.get(remote_file_path, local_file_path)
+
+    # Read the specific line
+    with open(local_file_path, 'r') as file:
+        lines = file.readlines()
+
+    line_content = lines[line_number - 1]
+
+    sftp.close()
+    transport.close()
+
+    return line_content
+
+# Call the function
+line_content = get_line_content(
+'192.168.15.34', 
+22, 
+'stack', 
+'123456', 
+'/opt/stack/nova/nova/objects/block_device.py', 
+90)
+print(line_content)
